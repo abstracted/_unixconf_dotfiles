cdd84c70e0522bcc6120d1ef1c5cfdb3f6af0ff6
Cameron <csanders@protonmail.com>
2020-10-22 01:17:22 -0500

    fixvscode extensions note

---
+++ scripts/fixvscode.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo "edit the following file"
+echo "/usr/lib/code/product.json"
+echo ""
+echo "find the following property"	
+echo "extensionsGallery"
+echo ""
+echo "add the following lines to that property"
+echo '"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",'
+echo '"cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",'
+echo '"itemUrl": "https://marketplace.visualstudio.com/items"'
+
