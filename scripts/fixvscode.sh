dfee39472ae438c4ed307dbbf36091648749de4a
Cameron <csanders@protonmail.com>
2020-12-06 14:41:48 -0600

    fix scripts

--- scripts/fixvscode.sh
+++ scripts/fixvscode.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+[[ `uname` == "Linux" ]] && PRODUCT_JSON="/usr/lib/code/product.json"
+[[ `uname` == "Darwin" ]] && PRODUCT_JSON="/Applications/VSCodium.app/Contents/Resources/app/product.json"
+
 echo "edit the following file"
-echo "/usr/lib/code/product.json"
+echo "$PRODUCT_JSON"
 echo ""
 echo "find the following property"	
 echo "extensionsGallery"
@@ -10,6 +13,6 @@ echo '"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
 echo '"cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",'
 echo '"itemUrl": "https://marketplace.visualstudio.com/items"'
 
-sudo sed -Ei 's/"serviceUrl".*$/"serviceUrl": "https:\/\/marketplace.visualstudio.com\/_apis\/public\/gallery",/g' /usr/lib/code/product.json
-sudo sed -Ei 's/"cacheUrl".*$/"cacheUrl": "https:\/\/vscode.blob.core.windows.net\/gallery\/index",/g' /usr/lib/code/product.json
-sudo sed -Ei 's/"itemUrl".*$/"itemUrl": "https:\/\/marketplace.visualstudio.com\/items"/g' /usr/lib/code/product.json
+sudo sed -E -i'' -e 's/"serviceUrl".*$/"serviceUrl": "https:\/\/marketplace.visualstudio.com\/_apis\/public\/gallery",/g' $PRODUCT_JSON
+sudo sed -E -i'' -e 's/"cacheUrl".*$/"cacheUrl": "https:\/\/vscode.blob.core.windows.net\/gallery\/index",/g' $PRODUCT_JSON
+sudo sed -E -i'' -e 's/"itemUrl".*$/"itemUrl": "https:\/\/marketplace.visualstudio.com\/items"/g' $PRODUCT_JSON
